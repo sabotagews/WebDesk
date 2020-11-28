0cb7c2c0cb45f417371b471c77723280b4f93b5c
Airwalk <airwalk@quattro.ws>
2020-11-27 21:05:41 -0600

    ConteoProveedores

--- includes/classes/mysql.cls.php
+++ includes/classes/mysql.cls.php
@@ -14,7 +14,7 @@ define('RESERVACION_SERVICIOS'		, array( 'AL' => 'Alojamiento', 'CH' => 'Charter
 define('RESERVACION_STATUS'			, array( '0' => 'Cotización', '1' => 'Confirmada', '2' => 'Pagada', '3' => 'Cobrada' ) );
 
 
-if( 0 ) {
+if( $_SERVER['SERVER_NAME'] == 'localhost' ) {
 
 	define('DB_HOSTNAME'	, 'localhost'				);
 	define('DB_USERNAME'	, 'root'					);
