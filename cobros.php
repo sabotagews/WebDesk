9752123a42da1381c4cb1cec59504c51a0669d40
Sabo Balleza <sabo@sabotage.ws>
2020-12-23 19:41:35 -0600

    Cobros

--- cobros.php
+++ cobros.php
@@ -65,7 +65,7 @@ window.onload = function( ) {
 }
 
 </script>
-<main class="container-fluid p-5" role="main">
+<main class="container" role="main">
 	<div class="py-5 text-center">
 		<h2>Cobros</h2>
 		<p class="lead">Captura de cobros ( pagos de clientes ) de <strong>reservaci&oacute;n.</strong></p>
@@ -76,8 +76,8 @@ window.onload = function( ) {
 	</form>
 	<div class="row" id="contenedor_reservacion" style="display: none">
 		<hr class="col-12 mt-4">
-		<div class="d-flex col-12 mt-4">
-			<div class="mr-auto">
+		<div class="d-flex col-12 mt-4 p-0">
+			<div class="mr-auto p">
 				<h1 id="cliente"></h1>
 			</div>
 			<div>
@@ -85,7 +85,7 @@ window.onload = function( ) {
 			</div>
 		</div>
 
-		<div class="col-12">
+		<div class="col-12 m-auto">
 
 			<form id="form_cobro" class="needs-validation" novalidate="">
 
@@ -93,104 +93,60 @@ window.onload = function( ) {
 				<input type="hidden" name="cobroId" value="0" />
 
 				<!--Detalle-->
-				<div class="form-row">
-					<div class="col-md-6 mb-3">
-						<label for="reservacionLocalizador">Localizador</label>
-						<span id="reservacionLocalizador"></span>
-					</div>
-					<div class="col-md-6 mb-3">
-						<label for="reservacionLocalizador">Saldo</label>
-						<span id="reservacionSaldo"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionProveedor">Proveedor</label>
-						<span id="reservacionProveedor"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="cliente">Cliente</label>
-						<span id="cliente"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionServicio">Servicio</label>
-						<span id="reservacionServicio"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionDestino">Destino</label>
-						<span id="reservacionDestino"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionHotel">Hotel</label>
-						<span id="reservacionHotel"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionPlan">Plan</label>
-						<span id="reservacionPlan"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionCheckIn">CheckIn</label>
-						<span id="reservacionCheckIn"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionCheckOut">CheckOut</label>
-						<span id="reservacionCheckOut"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionHabitaciones">Habitaciones</label>
-						<span id="reservacionHabitaciones"></span>
-	                </div>
-
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionDetalle">Detalle</label>
-						<span id="reservacionDetalle"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionCoste">Coste</label>
-						<span id="reservacionCoste"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionPrecio">Precio</label>
-						<span id="reservacionPrecio"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-	                    <label for="reservacionStatusCobro">Status de Cobro</label>
-						<span id="reservacionStatusCobro"></span>
-	                </div>
-					<div class="col-md-6 mb-3">
-						<label for="reservacionStatusPago">Status de Pago</label>
-						<label for="reservacionStatusPago">Status de Pago</label>
-						<span id="reservacionStatusPago"></span>
-					</div>
-
+				<div class="row">
+					<p class="col-6 p-0">
+						<span class="text-muted">Hotel: </span><strong id="reservacionHotel"></strong><br />
+						<span class="text-muted">CheckIn: </span><strong id="reservacionCheckIn"></strong><br />
+						<span class="text-muted">CheckOut: </span><strong id="reservacionCheckOut"></strong><br />
+						<span class="text-muted">Destino: </span><strong id="reservacionDestino"></strong><br />
+						<span class="text-muted">Plan: </span><strong id="reservacionPlan"></strong><br />
+						<span class="text-muted">Servicio: </span><strong id="reservacionServicio"></strong><br />
+						<span class="text-muted">Habitaciones: </span><strong id="reservacionHabitaciones"></strong><br />
+					</p>
+					<p class="col-6 text-right p-0">
+						<span class="text-muted">Proveedor: </span><strong id="reservacionProveedor"></strong><br />
+						<span class="text-muted">Coste: </span><strong id="reservacionCoste"></strong><br />
+						<span class="text-muted">Precio: </span><strong id="reservacionPrecio"></strong><br />
+						<span class="text-muted">Saldo: </span><strong id="reservacionSaldo"></strong><br />
+						<span class="text-muted">Cobro: </span><strong id="reservacionStatusCobro"></strong><br />
+						<span class="text-muted">Pago: </span><strong id="reservacionStatusPago"></strong><br />
+					</p>
+					<hr class="col-12">
+					<h5 class="text-muted">Detalle de la Reservaci&oacute;n</h5>
+					<p class="col-12 p-0" id="reservacionDetalle">
+					</p>
+					<hr class="col-12">
 				</div>
 
 				<!--Inputs-->
 				<div class="form-row">
-					<div class="col-md-6 mb-3">
-						<label for="cobroFechaAplicacion">Fecha Aplicación</label>
-						<input type="date" class="form-control" id="cobroFechaAplicacion" name="cobroFechaAplicacion" placeholder="dd/mm/yyyy" pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)" required>
-					</div>
-					<div class="col-md-6 mb-3">
-						<label for="cobroTipo">Cobro tipo</label>
-						<select class="custom-select" id="cobroTipo" onchange="" required>
-							<option></option>
-							<? foreach( COBRO_TIPOS as $k => $v ) { ?>
-								<option value="<?= $k; ?>"><?= $v; ?></option>
-							<? } ?>
-						</select>
-						<div class="invalid-feedback">
-							Seleccione la forma de cobro.
+					<div class="col-2">
+						<div class="col">
+							<label for="cobroFechaAplicacion">Fecha Aplicación</label>
+							<input type="date" class="form-control" id="cobroFechaAplicacion" name="cobroFechaAplicacion" placeholder="dd/mm/yyyy" pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)" required>
 						</div>
-					</div>
-					<div class="col-md-6 mb-3">
-						<label for="clienteApellido">Monto</label>
-						<input type="text" class="form-control" id="cobroMonto" placeholder="" value="" required="">
-						<div class="invalid-feedback">
-							El monto no es correcto.
+						<div class="col">
+							<label for="cobroTipo">Cobro tipo</label>
+							<select class="custom-select" id="cobroTipo" onchange="" required>
+								<option></option>
+								<? foreach( COBRO_TIPOS as $k => $v ) { ?>
+									<option value="<?= $k; ?>"><?= $v; ?></option>
+								<? } ?>
+							</select>
+							<div class="invalid-feedback">
+								Seleccione la forma de cobro.
+							</div>
 						</div>
+						<div class="col">
+							<label for="cobroMonto">Monto</label>
+							<input type="text" class="form-control" id="cobroMonto" placeholder="" value="" required="">
+							<div class="invalid-feedback">
+								El monto no es correcto.
+							</div>
+						</div>
+
 					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12 mb-3">
+					<div class="col-10">
 						<label for="cobroDetalle">Detalle</label>
 						<textarea class="form-control" id="cobroDetalle" name="cobroDetalle" rows="15"></textarea>
 					</div>
@@ -198,7 +154,7 @@ window.onload = function( ) {
 
 				<!--Botones-->
 				<hr class="mb-4">
-				<div class="form-row">
+				<div class="form-row text-right">
 					<div class="col-2" id="btn_nuevo" style="display: none">
 						<button class="btn btn-danger btn-lg btn-block" type="button" onclick="limpia_cobro( );">Nuevo</button>
 					</div>
